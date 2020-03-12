d4875ed204a18ab46c0c6be02da5ad9f67a1bcde
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-03-12 19:15:22 +0100

    Fixed a few bugs and Alexa integration!

--- ESP8266/NEOPIXELController/NEOPIXELController.ino
+++ ESP8266/NEOPIXELController/NEOPIXELController.ino
@@ -1,26 +1,3 @@
-/*
-  Welcome to the NebulaSmatLight Ardiono code. It is currently under development and the
-  Nextion screen implementetion has not complete yet. Soon TM :)
-
-  To-do:
-
-  [x]Implement ESP->Nextion communication
-  [x]Implement On/Off buttons Nextion
-  [x]Implement brightness control on the Nextion
-  [x]Implement Wifi status on Nextion
-  []Implement colour reproduction on Nextion
-
-  MIT License
-
-  Copyright (c) 2020 Eduardo M
-
-  VERSION: 0.6
-
-  DATE: 15/02/2020
-
-  Author: Eduardo M (Github: Aguilaair)
-*/
-
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
@@ -38,8 +15,7 @@ WebSocketsClient webSocket;
 WiFiClient client;
 
 #define LED_PIN    D1
-#define LED_COUNT 10
-#define LED1 "5e4a94180aa6af07008366e4"
+#define LED_COUNT 50
 
 
 
@@ -48,6 +24,13 @@ int G_Addr = 10;
 int B_Addr = 15;
 int Br_Addr = 20;
 int on_Addr = 25;
+
+int rgb_colors[3];
+
+float hue;
+double saturation;
+double bri;
+int tempK;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
@@ -63,12 +46,14 @@ int blue = 255;
 int brightness = 255;
 bool on = true;
 
-#define MyApiKey "66470be6-244b-4602-9c5e-xxxxxx" // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
-#define MySSID "D-xxxx" // TODO: Change to your Wifi network SSID
-#define MyWifiPassword "xxxx" // TODO: Change to your Wifi network password
+#define MyApiKey "66470be6-244b-4602-9c5e-eec7a2d15aa5" // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
+#define MySSID "D-LINK" // TODO: Change to your Wifi network SSID
+#define MyWifiPassword "12331233" // TODO: Change to your Wifi network password
 
 #define API_ENDPOINT "http://sinric.com"
 #define HEARTBEAT_INTERVAL 300000 // 5 Minutes 
+
+String devid = "5e5a4867a23b266b59a95ecf";
 
 uint64_t heartbeatTimestamp = 0;
 bool isConnected = false;
@@ -81,7 +66,7 @@ int incomingByte = 0; // for incoming serial data
 
 
 void turnOn(String deviceId) {
-  if (deviceId == LED1) // Device ID of first device
+  if (deviceId == devid) // Device ID of first device
   {
     on = true;
     Serial.print("Turn on device id: ");
@@ -100,7 +85,7 @@ void turnOn(String deviceId) {
 }
 
 void turnOff(String deviceId) {
-  if (deviceId == LED1) // Device ID of first device
+  if (deviceId == devid) // Device ID of first device
   {
     on = false;
     Serial.print("Turn off Device ID: ");
@@ -118,42 +103,27 @@ void turnOff(String deviceId) {
   }
 }
 
-void NextionUpdate() {
-  String message = myNextion.listen(); //check for message
-  if (message == "ON") {
-    turnOn(LED1);
-  }
-  else if (message == "OFF") {
-    turnOff(LED1);
-  }
-  else if (String(message).toInt() != 0) {
-    brightness = String(message).toInt();
-    NXsetBrightness(brightness);
-  }
-}
-
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
   switch (type) {
     case WStype_DISCONNECTED:
       isConnected = false;
       Serial.printf("[WSc] Webservice disconnected from sinric.com!\n");
-      myNextion.sendCommand("wifistatus.picc=1");
-      myNextion.sendCommand("colorpicker.wifistatus.picc=1");
       break;
     case WStype_CONNECTED: {
         isConnected = true;
         Serial.printf("[WSc] Service connected to sinric.com at url: %s\n", payload);
         Serial.printf("Waiting for commands from sinric.com ...\n");
+        chase(strip.Color(0, 255, 0));
+        chase(strip.Color(0, 255, 0));
+
         RGB_color(red, green, blue);
-        myNextion.sendCommand("main.wifistatus.picc=2");
-        myNextion.sendCommand("colorpicker.wifistatus.picc=2");
       }
       break;
     case WStype_TEXT: {
         Serial.printf("[WSc] get text: %s\n", payload);
 
         /*
-           {"deviceId":LED1,"action":"action.devices.commands.OnOff","value":{"on":true}}
+           {"deviceId":"5e28764500886b552b7519fc","action":"action.devices.commands.OnOff","value":{"on":true}}
 
         */
 
@@ -167,33 +137,70 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 #endif
         String deviceId = json ["deviceId"];
         String action = json ["action"];
-
-        if (action == "action.devices.commands.OnOff") { // Switch or Light
-          String value = json ["value"]["on"];
-          if (value == "true") {
-            turnOn(deviceId);
-          } else {
-            turnOff(deviceId);
+        if (deviceId == devid) // Device ID of first device
+        {
+          if (action == "action.devices.commands.OnOff") { // Switch or Light
+            String value = json ["value"]["on"];
+            if (value == "true") {
+              turnOn(deviceId);
+            } else {
+              turnOff(deviceId);
+            }
           }
-        }
-        else if (action == "action.devices.commands.ColorAbsolute") {
-          // Alexa, set the device name to red
-          // get text: {"deviceId":"xxxx","action":"SetColor","value":{"hue":0,"saturation":1,"brightness":1}}
-          int inOLE = json ["value"]["color"]["spectrumRGB"];
+          else if (action == "action.devices.commands.ColorAbsolute") {
+            // Alexa, set the device name to red
+            // get text: {"deviceId":"xxxx","action":"SetColor","value":{"hue":0,"saturation":1,"brightness":1}}
+            int inOLE = json ["value"]["color"]["spectrumRGB"];
 
-          Serial.println(inOLE);
-          OLEtoRBG(inOLE);
-          RGB_color(red, green, blue);
+            Serial.println(inOLE);
+            OLEtoRBG(inOLE);
+            RGB_color(red, green, blue);
 
-        }
-        else if (action == "action.devices.commands.BrightnessAbsolute") {
-          Serial.print("Brightness set to: ");
-          brightness = json ["value"]["brightness"];
-          setBrightness(brightness);
+          }
+          else if (action == "action.devices.commands.BrightnessAbsolute") {
+            Serial.print("Brightness set to: ");
+            brightness = json ["value"]["brightness"];
+            setBrightness(brightness);
 
-        }
-        else if (action == "test") {
-          Serial.println("[WSc] received test command from sinric.com");
+          }
+          //Alexa
+          else if (action == "setPowerState") { // Switch or Light
+            String value = json ["value"];
+            if (value == "ON") {
+              turnOn(deviceId);
+            } else {
+              turnOff(deviceId);
+            }
+          }
+          else if (action == "SetBrightness") {
+            Serial.print("Brightness set to: ");
+            brightness = json ["value"];
+            setBrightness(brightness);
+
+          }
+          else if (action == "SetColor") {
+            // Alexa, set the device name to red
+            // get text: {"deviceId":"xxxx","action":"SetColor","value":{"hue":0,"saturation":1,"brightness":1}}
+
+            hue = json ["value"]["hue"];
+            saturation = json ["value"]["saturation"];
+            bri = json ["value"]["brightness"];
+            getRGB(hue, saturation, bri);
+            RGB_color(red, green, blue);
+
+          }
+          else if (action == "SetColorTemperature") {
+            // Alexa, set the device name to red
+            // get text: {"deviceId":"xxxx","action":"SetColor","value":{"hue":0,"saturation":1,"brightness":1}}
+
+            tempK = json ["value"];
+            temp2rgb(tempK);
+            RGB_color(red, green, blue);
+
+          }
+          else if (action == "test") {
+            Serial.println("[WSc] received test command from sinric.com");
+          }
         }
       }
       break;
@@ -210,6 +217,87 @@ void OLEtoRBG(int ole) {
   blue = round(ole % 256);
 }
 
+void getRGB(int h, double s, double v) {
+  //this is the algorithm to convert from RGB to HSV
+  double r = 0;
+  double g = 0;
+  double b = 0;
+
+  double hf = h / 60.0;
+
+  int i = (int)floor(h / 60.0);
+  double f = h / 60.0 - i;
+  double pv = v * (1 - s);
+  double qv = v * (1 - s * f);
+  double tv = v * (1 - s * (1 - f));
+
+  switch (i)
+  {
+    case 0: //rojo dominante
+      r = v;
+      g = tv;
+      b = pv;
+      break;
+    case 1: //verde
+      r = qv;
+      g = v;
+      b = pv;
+      break;
+    case 2:
+      r = pv;
+      g = v;
+      b = tv;
+      break;
+    case 3: //azul
+      r = pv;
+      g = qv;
+      b = v;
+      break;
+    case 4:
+      r = tv;
+      g = pv;
+      b = v;
+      break;
+    case 5: //rojo
+      r = v;
+      g = pv;
+      b = qv;
+      break;
+  }
+
+  //set each component to a integer value between 0 and 255
+  red = constrain((int)255 * r, 0, 255);
+  green = constrain((int)255 * g, 0, 255);
+  blue = constrain((int)255 * b, 0, 255);
+}
+
+void temp2rgb(unsigned int kelvin) {
+  int temp = kelvin / 100;
+  int r, g, b;
+
+  if (temp <= 66) {
+    r = 255;
+    g = 99.4708025861 * log(temp) - 161.1195681661;
+    if (temp <= 19) {
+      b = 0;
+    } else {
+      b = 138.5177312231 * log(temp - 10) - 305.0447927307;
+    }
+  } else {
+    if (temp < 60) {
+      r = 0;
+      g = 0;
+    } else {
+      r = 329.698727446 * pow(temp - 60, -0.1332047592);
+      g = 288.1221695283 * pow(temp - 60, -0.0755148492);
+    }
+    b = 255;
+  }
+
+  red = (r > 0) ? r : 0;
+  green = (g > 0) ? g : 0;
+  blue = (b > 0) ? b : 0;
+}
 
 void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
 {
@@ -239,24 +327,32 @@ void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
   EEPROM.commit();
 }
 
+static void chase(uint32_t c) {
+  strip.clear();
+  strip.show();
+  for (uint16_t i = 0; i < strip.numPixels() + 4; i++) {
+    strip.setPixelColor(i  , c); // Draw new pixel
+    strip.setPixelColor(i - 4, 0); // Erase pixel a few steps back
+    strip.show();
+    delay(10);
+  }
+}
 
 void setBrightness(int br) {
+  EEPROM.put(Br_Addr, br);
+  EEPROM.commit();
+  Serial.print(br);
+  Serial.println("%");
   myNextion.sendCommand(("main.br.val=" + String(br)).c_str() );
   myNextion.sendCommand(("colorpicker.br.val=" + String(br)).c_str());
   myNextion.sendCommand(("colorpicker.brslider.val=" + String(br)).c_str());
-  int brm = br * 2.55;
-  strip.setBrightness(brm);
+  br = br * 2.55;
+  Serial.print("So, multiplier is: ");
+  Serial.println(br);
+  strip.setBrightness(br);
   strip.show();
-  EEPROM.put(Br_Addr, br);
-  EEPROM.commit();
-}
 
-void NXsetBrightness(int br) {
-  int brm = br * 2.55;
-  strip.setBrightness(brm);
-  strip.show();
-  EEPROM.put(Br_Addr, br);
-  EEPROM.commit();
+
 }
 
 void setup() {
@@ -291,24 +387,20 @@ void setup() {
   Serial.print("Connecting to Wifi: ");
   Serial.println(MySSID);
 
-  myNextion.sendCommand("page 1");
-  myNextion.sendCommand("main.wifistatus.picc=1");
-  myNextion.sendCommand("colorpicker.wifistatus.picc=1");
-
   // Waiting for Wifi connect
-  Serial.print("Connecting to WiFi...");
   while (WiFiMulti.run() != WL_CONNECTED) {
     delay(500);
-    NextionUpdate();
-  }
+    Serial.print(".");
+    chase(strip.Color(255, 0, 0));
 
+  }
   if (WiFiMulti.run() == WL_CONNECTED) {
     Serial.println("");
     Serial.print("WiFi connected. ");
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
-    myNextion.sendCommand("main.wifistatus.picc=2");
-    myNextion.sendCommand("colorpicker.wifistatus.picc=2"); 
+    myNextion.sendCommand("page 1");
+    myNextion.sendCommand("wifistatus.picc=2");
   }
 
   // server address, port and URL
@@ -334,7 +426,12 @@ void loop() {
     }
   }
 
-  NextionUpdate();
-
+  String message = myNextion.listen(); //check for message
+  if (message == "ON") {
+    turnOn("5e5a4867a23b266b59a95ecf");
+  }
+  if (message == "OFF") {
+    turnOff("5e5a4867a23b266b59a95ecf");
+  }
 
 }
